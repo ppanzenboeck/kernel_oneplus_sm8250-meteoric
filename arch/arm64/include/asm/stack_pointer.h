19842ff4292c25a628e10780bfecfba8781be9a9
xSiCxSmurf <160023457+xSiCxSmurf@users.noreply.github.com>
2025-08-22 00:56:26 -0500

    Update stack_pointer.h
    Stack_pointer.h fix

--- arch/arm64/include/asm/stack_pointer.h
+++ arch/arm64/include/asm/stack_pointer.h
@@ -5,6 +5,8 @@
 /*
  * how to get the current stack pointer from C
  */
+#if defined(__GNUC__) && !defined(__clang__)
 register unsigned long current_stack_pointer asm ("sp");
+#endif
 
 #endif /* __ASM_STACK_POINTER_H */
