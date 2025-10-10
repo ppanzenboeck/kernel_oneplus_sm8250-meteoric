1f681ffafda9194b52bdd3b5e50881bd3bfefcd4
oct888 <142801165+oct888@users.noreply.github.com>
2025-10-10 14:46:33 +0100

    clean up the anykernel script a bit

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Meteoric kernel by HELLBOY017, P.A.N.Z, oct888, KernalSU-Next + SuSFS 1.5.5 patches by Paradox
+kernel.string=Meteoric kernel by HELLBOY017, P.A.N.Z, Paradox, oct888
 do.devicecheck=0
 do.modules=0
 do.systemless=0
